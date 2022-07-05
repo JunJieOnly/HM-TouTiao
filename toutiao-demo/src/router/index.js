099658d6795dba1851d25e4afdbe0a0ad5d97dee
JunJie <2458780113@qq.com>
2022-07-05 10:33:52 +0800

    项目初始化

---
+++ toutiao-demo/src/router/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+// 路由映射数组
+const routes = []
+// 实例化路由对象
+const router = new VueRouter({
+  routes
+})
+
+export default router
