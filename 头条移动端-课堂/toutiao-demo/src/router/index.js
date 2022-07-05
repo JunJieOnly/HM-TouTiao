b4786bfadade34408b85a626558a7b7b0aacb960
JunJie <2458780113@qq.com>
2022-07-05 17:17:48 +0800

    配置基础路由

--- 头条移动端-课堂/toutiao-demo/src/router/index.js
+++ 头条移动端-课堂/toutiao-demo/src/router/index.js
@@ -4,7 +4,35 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 // 路由映射数组
-const routes = []
+const routes = [
+  {
+    path: "/",
+    redirect: "/home",
+    component: () => import("@/views/layout"),
+    children: [
+      {
+        path: "home",
+        component: () => import("@/views/layout/home")
+      },
+      {
+        path: "qa",
+        component: () => import("@/views/layout/qa")
+      },
+      {
+        path: "video",
+        component: () => import("@/views/layout/video")
+      },
+      {
+        path: "my",
+        component: () => import("@/views/layout/my")
+      },
+    ]
+  },
+  {
+    path: "/login",
+    component: () => import("@/views/login")
+  }
+]
 // 实例化路由对象
 const router = new VueRouter({
   routes
