ddd1c21231439486657b773d16273ce559e1d6d7
JunJie <2458780113@qq.com>
2022-07-05 23:25:27 +0800

    登录界面画好

--- 头条移动端-练习/hm-toutiao-demo/src/vant/index.js
+++ 头条移动端-练习/hm-toutiao-demo/src/vant/index.js
@@ -1,5 +1,9 @@
 import Vue from 'vue';
-import { Tabbar, TabbarItem } from 'vant';
+import { Tabbar, TabbarItem, Form, Field, NavBar, Button } from 'vant';
 
 Vue.use(Tabbar);
 Vue.use(TabbarItem);
+Vue.use(Form);
+Vue.use(Field);
+Vue.use(NavBar);
+Vue.use(Button);
--- 头条移动端-练习/hm-toutiao-demo/src/views/login/index.vue
+++ 头条移动端-练习/hm-toutiao-demo/src/views/login/index.vue
@@ -1,10 +1,37 @@
 <template>
-  <div>登录</div>
+  <div>
+    <!-- 导航栏 -->
+    <van-nav-bar title="登录" left-arrow />
+    <!-- 导航栏 -->
+    <!-- 表单 -->
+    <van-form @submit="onSubmit">
+      <van-field placeholder="请输入手机号">
+        <i slot="left-icon" class="toutiao toutiao-shouji"></i>
+      </van-field>
+      <van-field type="password" placeholder="请输入验证码">
+        <i slot="left-icon" class="toutiao toutiao-yanzhengma"></i>
+        <template #button>
+          <van-button size="small" type="primary">发送验证码</van-button>
+        </template>
+      </van-field>
+      <div style="margin: 16px">
+        <van-button block type="info" native-type="submit">登录</van-button>
+      </div>
+    </van-form>
+    <!-- 表单 -->
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    onSubmit() {},
+  },
+}
 </script>
 
 <style>
+.toutiao {
+  font-size: 37px;
+}
 </style>
