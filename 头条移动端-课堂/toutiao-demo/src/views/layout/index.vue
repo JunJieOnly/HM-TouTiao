f364e521dece02d398ccf23fccd221cfc79aa722
JunJie <2458780113@qq.com>
2022-07-05 18:54:12 +0800

    底部导航功能添加

--- 头条移动端-课堂/toutiao-demo/src/views/layout/index.vue
+++ 头条移动端-课堂/toutiao-demo/src/views/layout/index.vue
@@ -2,6 +2,21 @@
   <div>
     <!-- 二级路由 -->
     <router-view />
+    <van-tabbar route>
+      <!-- 用icon-prefix字体图标修改默认前缀 -->
+      <van-tabbar-item icon-prefix="toutiao" replace to="/home" icon="shouye"
+        >首页</van-tabbar-item
+      >
+      <van-tabbar-item icon-prefix="toutiao" replace to="/qa" icon="wenda"
+        >问答</van-tabbar-item
+      >
+      <van-tabbar-item icon-prefix="toutiao" replace to="/video" icon="shipin"
+        >视频</van-tabbar-item
+      >
+      <van-tabbar-item icon-prefix="toutiao" replace to="/my" icon="wode"
+        >我的</van-tabbar-item
+      >
+    </van-tabbar>
   </div>
 </template>
 
