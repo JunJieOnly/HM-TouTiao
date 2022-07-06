457f314e6787e05c861fa34e11421d5ff5acab40
JunJie <2458780113@qq.com>
2022-07-06 12:16:38 +0800

    基础表单验证

--- 头条移动端-练习/hm-toutiao-demo/src/views/login/index.vue
+++ 头条移动端-练习/hm-toutiao-demo/src/views/login/index.vue
@@ -5,17 +5,28 @@
     <!-- 导航栏 -->
     <!-- 表单 -->
     <van-form @submit="onSubmit">
-      <van-field placeholder="请输入手机号">
-        <i slot="left-icon" class="toutiao toutiao-shouji"></i>
-      </van-field>
-      <van-field type="password" placeholder="请输入验证码">
-        <i slot="left-icon" class="toutiao toutiao-yanzhengma"></i>
-        <template #button>
-          <van-button size="small" type="primary">发送验证码</van-button>
-        </template>
-      </van-field>
+      <van-field
+        v-model="user.mobile"
+        type="Number"
+        icon-prefix="toutiao"
+        left-icon="shouji"
+        maxlength="11"
+        name="mobile"
+        placeholder="请输入手机号"
+        :rules="rules.mobile"
+      />
+      <van-field
+        v-model="user.code"
+        type="Number"
+        icon-prefix="toutiao"
+        left-icon="yanzhengma"
+        maxlength="6"
+        name="code"
+        placeholder="请输入验证码"
+        :rules="rules.code"
+      />
       <div style="margin: 16px">
-        <van-button block type="info" native-type="submit">登录</van-button>
+        <van-button block type="info" native-type="submit">提交</van-button>
       </div>
     </van-form>
     <!-- 表单 -->
@@ -24,8 +35,28 @@
 
 <script>
 export default {
+  data() {
+    return {
+      user: {
+        mobile: "",
+        code: "",
+      },
+      rules: {
+        mobile: [
+          { required: true, message: "请填写手机号" },
+          { pattern: /^1[3|4|5|6|7|8|9|]\d{9}$/, message: "手机号格式不正确" },
+        ],
+        code: [
+          { required: true, message: "请填写验证码" },
+          { pattern: /\d{6}$/, message: "验证码格式不正确" },
+        ],
+      },
+    }
+  },
   methods: {
-    onSubmit() {},
+    onSubmit() {
+      console.log("验证通过，可以提交数据")
+    },
   },
 }
 </script>
