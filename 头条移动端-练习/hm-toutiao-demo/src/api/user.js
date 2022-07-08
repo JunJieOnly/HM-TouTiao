f8a622c96ab12688b508916997d56a3e7861df95
JunJie <2458780113@qq.com>
2022-07-08 23:30:45 +0800

    优化token

--- 头条移动端-练习/hm-toutiao-demo/src/api/user.js
+++ 头条移动端-练习/hm-toutiao-demo/src/api/user.js
@@ -1,9 +1,4 @@
 import request from "@/utils/request"
-import store from "@/store"
 export const UserInfoApi = () => request({
-    url: "/v1_0/user",
-    headers: {
-        Authorization: `Bearer ${store.getters.token}`
-
-    }
+    url: "/v1_0/user"
 })
--- 头条移动端-练习/hm-toutiao-demo/src/utils/request.js
+++ 头条移动端-练习/hm-toutiao-demo/src/utils/request.js
@@ -1,10 +1,19 @@
 import axios from "axios"
+// 导入vuex数据
+import store from "@/store"
 const instance = axios.create({
     baseURL: "http://toutiao.itheima.net"
 })
 // 添加请求拦截器
 instance.interceptors.request.use(function (config) {
     // 在发送请求之前做些什么
+    // console.log(config);
+    // 优化token
+    const { user } = store.state
+    if (user && user.token) {
+        config.headers.Authorization = `Bearer ${user.token}`
+    }
+    console.log(config);
     return config;
 }, function (error) {
     // 对请求错误做些什么
